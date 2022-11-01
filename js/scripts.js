0e9afcf3938038d521a569978664815b5872d5f6
kevintykim <114948536+kevintykim@users.noreply.github.com>
2022-11-01 16:37:42 -0700

    Add an interactive hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi there, and welcome to my portfolio site. Hope you enjoy your visit!");
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
